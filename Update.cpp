48a52d83074492d535310c33c690016d68db9d05
SCOTT CAMERON <1501366@student.fife.ac.uk>
2018-03-22 10:56:10 +0000

    implement detectCollisions

--- Update.cpp
+++ Update.cpp
@@ -18,36 +18,60 @@ void Engine::update(float dtAsSeconds)
 		m_Thomas.update(dtAsSeconds);
 		m_Bob.update(dtAsSeconds);
 
-		//count down the time the player has left
-		m_TimeRemaining -= dtAsSeconds;
-
-		//have thomas and bob run out of time
-		if (m_TimeRemaining <= 0)
+		//detect collisons and see if the character have reached the goal
+		if (detectCollisions(m_Thomas) && detectCollisions(m_Bob))
 		{
+			//new level required
 			m_NewLevelRequired = true;
+
+			//TODO: Play sound for reaching goal
 		}
-
-
-	}//end of if playing
-
-	//set the view around the appropriate characters
-	if (m_SplitScreen)
-	{
-		m_leftview.setCenter(m_Thomas.getCentre());
-		m_rightview.setCenter(m_Bob.getCentre());
-	}
-	else
-	{
-		if (m_Character1)	// if we should focus on thomas
+		else
 		{
-			m_mainview.setCenter(m_Thomas.getCentre());;
-		}
-		
-		if (m_Character1)	// if we should focus on Bob
+			//run bob collision detection
+			detectCollisions(m_Bob);
+
+			// let bob and thomas jump on each others heads
+			if (m_Bob.getFeet().intersects(m_Thomas.getHead()))
+			{
+				m_Bob.stopFalling(m_Thomas.getHead().top);
+			}
+
+			if (m_Thomas.getFeet().intersects(m_Bob.getHead()))
+			{
+				m_Thomas.stopFalling(m_Bob.getHead().top);
+			}
+
+			//count down the time the player has left
+			m_TimeRemaining -= dtAsSeconds;
+
+			//have thomas and bob run out of time
+			if (m_TimeRemaining <= 0)
+			{
+				m_NewLevelRequired = true;
+			}
+
+
+		}//end of if playing
+
+		//set the view around the appropriate characters
+		if (m_SplitScreen)
 		{
-			m_mainview.setCenter(m_Bob.getCentre());;
+			m_leftview.setCenter(m_Thomas.getCentre());
+			m_rightview.setCenter(m_Bob.getCentre());
+		}
+		else
+		{
+			if (m_Character1)	// if we should focus on thomas
+			{
+				m_mainview.setCenter(m_Thomas.getCentre());;
+			}
+
+			if (m_Character1)	// if we should focus on Bob
+			{
+				m_mainview.setCenter(m_Bob.getCentre());;
+			}
 		}
 	}
-
 
 }//end of update function
